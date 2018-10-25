5cca5876f9843ec6742b156fa9e305877f0bebe8
Namratha <Namratha@gbcilsa313grad02imac.dhcp.asu.edu>
2018-10-25 10:22:13 -0700

    [SUS-1] Adding minor formatting changes and styling.

--- sustainability/src/main/java/edu/asu/diging/sustainability/web/ListConceptController.java
+++ sustainability/src/main/java/edu/asu/diging/sustainability/web/ListConceptController.java
@@ -2,7 +2,7 @@ package edu.asu.diging.sustainability.web;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -15,23 +15,22 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import edu.asu.diging.sustainability.core.model.IConcept;
-import edu.asu.diging.sustainability.core.model.impl.Concept;
 import edu.asu.diging.sustainability.core.service.IConceptManager;
 
 @Controller
 public class ListConceptController {
-    
+
     @Autowired
     private IConceptManager conceptManager;
 
-    @RequestMapping(value="/admin/concept/list")
+    @RequestMapping(value = "/admin/concept/list")
     public String list(Model model) {
         return "admin/concept/list";
     }
-    
-    @RequestMapping(value="/admin/concept/list.json")
+
+    @RequestMapping(value = "/admin/concept/list.json")
     public ResponseEntity<String> listJson() {
-        
+
         ObjectMapper mapper = new ObjectMapper();
         ArrayNode concepts = mapper.createArrayNode();
         for (IConcept concept : conceptManager.getTopConcepts()) {
@@ -42,30 +41,31 @@ public class ListConceptController {
 
         return ResponseEntity.ok().headers(headers).body(concepts.toString());
     }
-    
+
     private void addNode(IConcept concept, ArrayNode conceptArray, ObjectMapper mapper) {
         ObjectNode conceptNode = mapper.createObjectNode();
         conceptNode.put("text", concept.getName());
         conceptArray.add(conceptNode);
-        
+
         if (concept.getChildren() != null) {
             ArrayNode children = mapper.createArrayNode();
             conceptNode.put("nodes", children);
             for (IConcept child : concept.getChildren()) {
                 addNode(child, children, mapper);
             }
-            
+
         }
     }
-    
-    @RequestMapping(value="/admin/concept/configuration")
+
+    @RequestMapping(value = "/admin/concept/configuration")
     public String configuration(Model model) {
-      List<IConcept> concepts = conceptManager.getTopConcepts();
-      model.addAttribute("concepts", concepts);
-      List<String> roles = new ArrayList<String>();
-      roles.add("Researcher");
-      roles.add("Practitioner");
-      model.addAttribute("roles", roles);
-      return "admin/concept/configuration";
+        List<IConcept> concepts = conceptManager.getTopConcepts();
+        model.addAttribute("concepts", concepts);
+        List<String> roles = new ArrayList<String>();
+        roles.add("Researcher");
+        roles.add("Practitioner");
+        model.addAttribute("roles", roles);
+        return "admin/concept/configuration";
     }
+
 }
--- sustainability/src/main/webapp/WEB-INF/views/admin/concepts/configuration.jsp
+++ sustainability/src/main/webapp/WEB-INF/views/admin/concepts/configuration.jsp
@@ -1,27 +1,29 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ taglib prefix="sec"
 	uri="http://www.springframework.org/security/tags"%>
-<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+
+<!-- Custom styling for configuration page. -->
+<link href="<c:url value="/resources/bootstrap/css/admin/admin.css" />" rel="stylesheet">
 
 <div class="panel panel-default">
   <c:forEach items="${concepts}" var="concept">
     <div class="panel-heading">
       <p><strong>${concept.name}</strong></p>
     </div>
+    
     <div>
       <c:forEach items="${concept.children}" var="child">
         <div class="panel-body"">
-        <li class="list-group-item">
-          ${child.name}
-          <c:forEach items="${roles}" var="role">
-            <label class="switch ">
-              <input type="checkbox" class="default" name="${role}" value="${role}"> ${role}
-            </label>
-          </c:forEach>
-        </li>
+	      <li class="list-group-item">
+	        ${child.name}
+	        <c:forEach items="${roles}" var="role">
+	          <label class="switch checkbox-switch">
+	            <input type="checkbox" class="default" name="${role}" value="${role}"> ${role}
+	          </label>
+	        </c:forEach>
+	      </li>
         </div>
       </c:forEach>
     </div>
