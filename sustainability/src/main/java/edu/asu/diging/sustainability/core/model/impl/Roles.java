7c4fe436c4af0902bb551560a6e8a30b91f4faa4
Namratha Olety Venkatesh <namrathaov@gmail.com>
2018-11-09 14:19:26 -0700

    Updated author

--- sustainability/src/main/java/edu/asu/diging/sustainability/core/model/impl/Roles.java
+++ sustainability/src/main/java/edu/asu/diging/sustainability/core/model/impl/Roles.java
@@ -2,7 +2,7 @@ package edu.asu.diging.sustainability.core.model.impl;
 
 /**
  * Enumerator to configure the roles for accessing Concepts.
- * @author Namratha
+ * @author namrathaov
  *
  */
 public enum Roles {
