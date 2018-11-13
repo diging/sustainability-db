91909de7ea2af30ee0f564644c466595ed84f1dc
Namratha <Namratha@gbcilsa313grad02imac.dhcp.asu.edu>
2018-11-13 14:24:30 -0700

    [SUS-15] Saving work to make ajax call to load resource.

--- sustainability/src/main/java/edu/asu/diging/sustainability/web/search/ResearcherSearchController.java
+++ sustainability/src/main/java/edu/asu/diging/sustainability/web/search/ResearcherSearchController.java
@@ -4,14 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.ResponseBody;
 import edu.asu.diging.sustainability.core.data.ConceptRepository;
 import edu.asu.diging.sustainability.core.exception.NotAValidResourceException;
 import edu.asu.diging.sustainability.core.model.IAnnotation;
@@ -23,21 +22,22 @@ import edu.asu.diging.sustainability.core.service.IResourceManager;
 
 @Controller
 public class ResearcherSearchController {
-    
+
     @Autowired
     private IAnnotationManager annotationManager;
-    
+
     @Autowired
     private ConceptRepository conceptRepo;
-    
+
     @Autowired
     private IResourceManager resourceManager;
-    
-    @RequestMapping(value="/perspective/search", method=RequestMethod.POST)
+
+    @RequestMapping(value = "/perspective/search", method = RequestMethod.POST)
     public String search(Model model, @RequestParam("selectedConcepts") String[] selectedConcepts) {
-    	Map<String, List<IAnnotation>> URIList = annotationManager.findTextsForConcepts(selectedConcepts);
+        Map<String, List<IAnnotation>> URIList =
+                annotationManager.findTextsForConcepts(selectedConcepts);
         List<IResource> resourceList = new ArrayList<IResource>();
-        for(String s: URIList.keySet()) {
+        for (String s : URIList.keySet()) {
             try {
                 resourceList.add(resourceManager.getResource(s));
             } catch (NotAValidResourceException e) {
@@ -45,9 +45,9 @@ public class ResearcherSearchController {
                 e.printStackTrace();
             }
         }
-        model.addAttribute("resource",resourceList);
-        //model.addAttribute("results", annotationManager.findTextsForConcepts(selectedConcepts));
-        
+        model.addAttribute("resource", resourceList);
+        // model.addAttribute("results", annotationManager.findTextsForConcepts(selectedConcepts));
+
         List<IConcept> concepts = new ArrayList<>();
         for (String conceptId : selectedConcepts) {
             Optional<Concept> concept = conceptRepo.findById(conceptId);
@@ -56,19 +56,21 @@ public class ResearcherSearchController {
             }
         }
         model.addAttribute("concepts", concepts);
-        
+
         return "perspective/researcher/search";
     }
-    
-    @RequestMapping(value="/perspective/researcher/resource", method=RequestMethod.GET)
+
+    @RequestMapping(value = "/perspective/researcher/resource", method = RequestMethod.GET)
+    @ResponseBody
     public IResource reload(Model model, @RequestParam("uri") String uri) {
-    		System.out.println("uri "+uri);
-    		try {
-                return resourceManager.getResource(uri);
-            } catch (NotAValidResourceException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-    		return null;
+        System.out.println("uri " + uri);
+        try {
+            model.addAttribute("rsrc", resourceManager.getResource(uri));
+            return resourceManager.getResource(uri);
+        } catch (NotAValidResourceException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
     }
 }
--- sustainability/src/main/webapp/WEB-INF/views/search/results.jsp
+++ sustainability/src/main/webapp/WEB-INF/views/search/results.jsp
@@ -10,38 +10,37 @@
 </h1>
 
 <ul class="list-group">
-	<c:forEach items="${resource}" var="entry">
+	<c:forEach items="${resource}" var="entry" varStatus="vs">
 		<c:if test="${empty entry.updatedOn}">
-			<li id="part" class="list-group-item">
-			<i class="fa fa-spinner fa-pulse"></i> Please wait while we are loading the text for you.
-			
-		</li>
-		<script>
+			<li id="part${vs.index}" class="list-group-item"><i
+				class="fa fa-spinner fa-pulse"></i> Please wait while we are loading
+				the text for you.</li>
+			<script>
 		$(document).ready(function() {
-			$.ajax({
-				url: '/perspective/researcher/resource',
+			setTimeout(function() 
+					  {
+				$.ajax({
+				url: "<c:url value="/perspective/researcher/resource"/>",
 				type : 'GET',
 				data: { 'uri' : '${entry.uri}'},
-				headers: {
-					"Accept": "text/plain; charset=utf-8",         
-				    "Content-Type": "text/plain; charset=utf-8"	
-				},
-				success: function (resource) {
-					console.log(resource);
-					/* setInterval(function() 
-							  {
-						$('part').html("<a href='<c:url value='/text?uri="+resource.uri+"'/> >"+resource.title - resource.year+"</a>");
-							  }, 3000); */
+				success: function (rsrc) {
+					console.log(rsrc);
+					console.log(${vs.index});
+					
+					$(part${vs.index}).html("<a href='<c:url value='/text?uri="+rsrc.uri+"'/> >"+rsrc.title+"</a>");
 				},
 				error: function(e){
 					console.log('Error');
 				}
 			}); 
+			},  3000);
 		});
 		</script>
 		</c:if>
 		<c:if test="${not empty entry.updatedOn}">
-			<li class="list-group-item"><a href="<c:url value="/text?uri=${entry.uri}"/>" >${entry.title} - ${entry.year} </a></li>
+			<li class="list-group-item"><a
+				href="<c:url value="/text?uri=${entry.uri}"/>">${entry.title} -
+					${entry.year} </a></li>
 		</c:if>
 	</c:forEach>
 </ul>
