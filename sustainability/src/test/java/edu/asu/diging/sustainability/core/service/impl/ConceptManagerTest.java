cf8b39f6171688b8456dd9b896c370e93750302a
Namratha <Namratha@gbcilsa313grad02imac.dhcp.asu.edu>
2019-01-15 11:54:02 -0700

    [SUS-13] Adding test for store concept fields.

--- sustainability/src/test/java/edu/asu/diging/sustainability/core/service/impl/ConceptManagerTest.java
+++ sustainability/src/test/java/edu/asu/diging/sustainability/core/service/impl/ConceptManagerTest.java
@@ -1,34 +1,45 @@
 package edu.asu.diging.sustainability.core.service.impl;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import edu.asu.diging.sustainability.core.data.ConceptRepository;
-import edu.asu.diging.sustainability.core.model.IConcept;
+import edu.asu.diging.sustainability.core.model.SearchCategory;
 import edu.asu.diging.sustainability.core.model.impl.Concept;
+import static org.mockito.Mockito.verify;
 
 public class ConceptManagerTest {
 
+    private String alias = "concept";
+    private List<SearchCategory> categoryList = new ArrayList<SearchCategory>();
     @Mock
     private ConceptRepository conceptRepo;
     
     @InjectMocks
     private ConceptManager conceptManager = new ConceptManager();
+
+    private Concept concept = new Concept();
     
     @Before
     public void init() {
         MockitoAnnotations.initMocks(this);
+        concept.setId("ConceptId1");
     }
     
+    @Test
     public void test_storeConceptSearchCategoriesAndAlias_success() {
-        Concept concept = new Concept();
-        conceptRepo.save(concept);
-        IConcept mockConcept = conceptManager.getConceptById(concept.getId());
-        mockConcept.setSearchCategories(new ArrayList<>());
-        mockConcept.setAlias(Mockito.anyString());
-        Mockito.when(conceptRepo.save((Concept)mockConcept)).thenReturn((Concept)mockConcept);
+        concept.setSearchCategories(categoryList);
+        concept.setAlias(alias);
+        Mockito.when(conceptRepo.findById("ConceptId1")).thenReturn(Optional.of(concept));
+        Mockito.when(conceptRepo.save(concept)).thenReturn(concept);
+        conceptManager.storeConceptSearchCategoriesAndAlias(concept.getId(), categoryList, alias);
+        verify(conceptRepo).save(concept);
     }
+
 }
